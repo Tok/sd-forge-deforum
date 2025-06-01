404c7ca9b89e8914aac9a697b8a3f8c7fbd04a44
DESK\Zirteq <zirteq111@gmail.com>
2025-06-01 23:46:31 +0200

    Fix webui_sd_pipeline import path in main_generation_pipeline.py

--- deforum/core/main_generation_pipeline.py
+++ deforum/core/main_generation_pipeline.py
@@ -9,7 +9,7 @@ from modules import processing, sd_models
 from modules.shared import sd_model, state, cmd_opts
 from ..integrations.controlnet.core_integration import is_controlnet_enabled, get_controlnet_script_args
 from ..media.image_loading import load_img, prepare_mask, check_mask_for_errors
-from .webui_sd_pipeline import get_webui_sd_pipeline
+from ..integrations.webui_pipeline import get_webui_sd_pipeline
 from ..utils.rich import console
 from .defaults import get_samplers_list, get_schedulers_list
 from ..prompt import check_is_number
