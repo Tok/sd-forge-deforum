545f24c44d10e7931fa14a12393c65494ffc92bd
DESK\Zirteq <zirteq111@gmail.com>
2025-06-01 23:38:53 +0200

    Add missing CYAN color constant

--- deforum/utils/color_constants.py
+++ deforum/utils/color_constants.py
@@ -14,6 +14,7 @@ HEX_ORANGE = '#FFB750'
 HEX_YELLOW = '#FFEA56'
 HEX_GREEN = '#8FE968'
 HEX_BLUE = '#36CEDC'
+HEX_CYAN = '#00FFFF'
 HEX_PURPLE = '#A587CA'
 
 
@@ -31,6 +32,7 @@ ORANGE = from_hex_color(HEX_ORANGE)
 YELLOW = from_hex_color(HEX_YELLOW)
 GREEN = from_hex_color(HEX_GREEN)
 BLUE = from_hex_color(HEX_BLUE)
+CYAN = from_hex_color(HEX_CYAN)
 PURPLE = from_hex_color(HEX_PURPLE)
 
 BOLD = f"{ESC}1{TERM}"
